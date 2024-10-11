347dbcb933c57b34ec263c462f5c5ad8b097cf90
tatu-dev-labs <tatu.dev.lab@gmail.com>
2024-10-11 13:46:31 +0200

    Section 2: Layout Components - Screen Splitter

--- code/src/App.js
+++ code/src/App.js
@@ -1,5 +1,15 @@
+import { SplitScreen } from "./components/SplitScreen";
+
+const LeftComponent = () => {
+  return <h2 style={{backgroundColor: 'crimson'}}>I am Left</h2>
+}
+
+const RightComponent = () => {
+  return <h2 style={{backgroundColor: 'burlywood'}}>I am Right</h2>
+}
+
 function App() {
-  return <div>Advanced React</div>;
+  return <SplitScreen Left={LeftComponent} Right={RightComponent} />;
 }
 
 export default App;
