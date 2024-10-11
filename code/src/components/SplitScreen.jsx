08f6ff07e577855451bd59a13c42d5b20573ce5c
tatu-dev-labs <tatu.dev.lab@gmail.com>
2024-10-11 13:49:34 +0200

    Section 2: Layout Components - Screen Splitter: Add code comments for document the component

--- code/src/components/SplitScreen.jsx
+++ code/src/components/SplitScreen.jsx
@@ -6,7 +6,16 @@ const Container = styled.div`
 const Panel = styled.div`
   flex: 1;
 `
+/*
+  Usage:
+  <SplitScreen Left={LeftComponent} Right={RightComponent} />
 
+  LeftComponent and RightComponent are components that will be rendered 
+  in the left and right panels respectively.
+
+  The SplitScreen component is a layout component that takes 
+  two components
+*/
 export const SplitScreen = ({Left, Right}) => {
   return (
     <Container>
