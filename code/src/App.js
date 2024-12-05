f54b62a6d3e0c0d4ac00e7d5c3128fd55b2a5ae4
tatu-dev-labs <tatu.dev.labs@gmail.com>
2024-12-05 10:23:18 +0100

    Section 2: Layout Components - Backup App.js an clean the original App.js file

--- code/src/App.js
+++ code/src/App.js
@@ -1,20 +1,5 @@
-import { SplitScreen } from "./components/SplitScreen";
-
-const LeftComponent = ({ title }) => {
-  return <h2 style={{ backgroundColor: "crimson" }}>{title}</h2>;
-};
-
-const RightComponent = ({ title }) => {
-  return <h2 style={{ backgroundColor: "burlywood" }}>{title}</h2>;
-};
-
 function App() {
-  return (
-    <SplitScreen leftWidth={1} rightWidth={3}>
-      <LeftComponent title="Left" />
-      <RightComponent title="Right" />
-    </SplitScreen>
-  );
+  return <></>;
 }
 
 export default App;
