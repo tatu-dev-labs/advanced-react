57442a416110a55e4d6c6a7c36ea6696f35bf69d
tatu-dev-labs <tatu.dev.labs@gmail.com>
2024-12-10 08:29:48 +0100

    Section 2: Layout Components - Modals - App.js backup

--- code/src/App.js
+++ code/src/App.js
@@ -1,20 +1,5 @@
-import { Modal } from "./components/Modal";
-import { RegularList } from "./components/lists/RegularList";
-import { LargeBooksListItems } from "./components/books/LargeListItems";
-import { books } from "./data/books";
-
 function App() {
-  return (
-    <>
-      <Modal>
-        <RegularList
-          items={books}
-          sourceName="book"
-          ItemComponent={LargeBooksListItems}
-        />
-      </Modal>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
