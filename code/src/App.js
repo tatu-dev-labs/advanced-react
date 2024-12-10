ace1541bb1a31e9b4ab19bd37cbf6f78205a46c2
tatu-dev-labs <tatu.dev.labs@gmail.com>
2024-12-10 08:00:15 +0100

    Section 2: Layout Components - Lists types - App.js backup

--- code/src/App.js
+++ code/src/App.js
@@ -1,40 +1,5 @@
-import { LargeAuthorsListItems } from "./components/authors/LargeListItems";
-import { SmallAuthorsListItems } from "./components/authors/SmallListItems";
-import { LargeBooksListItems } from "./components/books/LargeListItems";
-import { SmallBooksListItems } from "./components/books/SmallListItems";
-import { NumberedList } from "./components/lists/NumberedList";
-import { RegularList } from "./components/lists/RegularList";
-import { authors } from "./data/authors";
-import { books } from "./data/books";
-
 function App() {
-  return (
-    <>
-      <RegularList
-        items={authors}
-        sourceName="author"
-        ItemComponent={SmallAuthorsListItems}
-      />
-      <hr />
-      <RegularList
-        items={authors}
-        sourceName="author"
-        ItemComponent={LargeAuthorsListItems}
-      />
-      <hr />
-      <RegularList
-        items={books}
-        sourceName="book"
-        ItemComponent={SmallBooksListItems}
-      />
-      <hr />
-      <NumberedList
-        items={books}
-        sourceName="book"
-        ItemComponent={LargeBooksListItems}
-      />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
